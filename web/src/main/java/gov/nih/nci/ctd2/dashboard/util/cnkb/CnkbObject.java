bd2f48cff54b822e26c4e0c94f8061a43c817d09
Zhou Ji <zj2122@columbia.edu>
2018-01-26 17:05:45 -0500

    remove CNKB-related code

--- web/src/main/java/gov/nih/nci/ctd2/dashboard/util/cnkb/CnkbObject.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.ctd2.dashboard.util.cnkb;
-
-import java.io.Serializable;
-
-public class CnkbObject implements Serializable {
- 
-	private static final long serialVersionUID = 3733149814707469271L;
-
-}
