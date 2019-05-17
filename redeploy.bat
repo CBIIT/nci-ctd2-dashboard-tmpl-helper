be86b7d228a7c1407115d03b40c2398643b4f525
Zhou Ji <zj2122@columbia.edu>
2019-05-17 16:14:28 -0400

    minor update of local development script

--- redeploy.bat
+++ redeploy.bat
@@ -1,9 +1,11 @@
 rem Windows script to rebuild the whole project and redeploy to local tomcat
 
-rem CATALINA_HOME needs to be set to run this
+IF NOT DEFINED MYSQL_PORT SET MYSQL_PORT=3306
+IF NOT DEFINED DB_PASSWORD SET DB_PASSWORD=admin
+IF NOT DEFINED CATALINA_HOME SET CATALINA_HOME=C:\apache-tomcat-8.5.34
 
 set start=%time%
-call mvn -f ./web/pom.xml clean package
+call mvn -f ./web/pom.xml -Ddatabase.password=%DB_PASSWORD% -Dupload.location=/ctd2upload/ -Dsubmission.builder.version=v2.1 clean package
 if errorlevel 1 (
     echo Failure Reason Given is %errorlevel%
     exit /b %errorlevel%
